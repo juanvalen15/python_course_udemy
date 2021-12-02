5ac68045c2f4f709e1d10ddc8c02ddb2f2364a66
juanvalen15 <juanvalen15@gmail.com>
2021-12-02 22:23:49 +0100

    show data types
    tyoe()

---
+++ python_basics/basics.py
@@ -0,0 +1,15 @@
+day_hours = 24
+week_days = 7
+
+week_hours = week_days * day_hours
+print(week_hours)
+
+x = 10
+y = "10"
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
