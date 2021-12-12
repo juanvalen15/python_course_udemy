0cccf0dc660ddd364aa09280d83a91456d9d5786
juanvalen15 <juanvalen15@gmail.com>
2021-12-12 15:05:25 +0100

    operations with dictionaries
    operations with dictionaries

--- python_basics/operations_with_lists.py
+++ python_basics/operations_with_lists.py
@@ -17,3 +17,12 @@ print(monday_temperatures.index(8.8,1,2)) # limiting the index search from eleme
 print(monday_temperatures.__getitem__(1))
 print(monday_temperatures[1])
 
+# accessing lists
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[0:2])
+print(monday_temperatures[-2])
+print(monday_temperatures[-2:])
+
+monday_temperatures = ["cold", 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[0][2:4]) # last index [2:4] is not included in the sliced list. That is index [2:4] -> elements 2 and 3
+
