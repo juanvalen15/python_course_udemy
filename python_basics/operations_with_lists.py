e09741f45b53d05a5f1f3cf5d3bd8a9188859366
juanvalen15 <juanvalen15@gmail.com>
2021-12-12 14:25:19 +0100

    __getitem__ method
    method use

--- python_basics/operations_with_lists.py
+++ python_basics/operations_with_lists.py
@@ -12,3 +12,8 @@ print(monday_temperatures)
 monday_temperatures = [9.1, 8.8, 7.5]
 print(monday_temperatures.index(8.8))
 print(monday_temperatures.index(8.8,1,2)) # limiting the index search from element 1 to element 2 of the list. Skipping element 0
+
+# __getitem__
+print(monday_temperatures.__getitem__(1))
+print(monday_temperatures[1])
+
