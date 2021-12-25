8a5e994c8c9b94212eef5b7ac1d5b655adf31faa
juanvalen15 <juanvalen15@gmail.com>
2021-12-25 18:39:04 +0100

    adding exceptions in list comprehensions

--- python_basics/list_comprehension.py
+++ python_basics/list_comprehension.py
@@ -11,5 +11,8 @@ print(new_temps)
 
 # using list comprehension
 new_temps = [temp / 10 for temp in temps]
+print(new_temps)
 
+temps = [221, 234, 340, 212, -9999]
+new_temps = [temp / 10 for temp in temps if temp != -9999]
 print(new_temps)
